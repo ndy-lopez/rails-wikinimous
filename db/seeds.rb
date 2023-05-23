2b0cdc8f4ca3bd3edf4b4aeb43719f03c5feced5
Andy Lopez <ndy.loredo@gmail.com>
2023-05-23 16:43:17 -0400

    complete article challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Cleaning DB"
+Article.destroy_all
+puts "Creating 10 articles..."
+
+10.times do
+  article = Article.new(
+    title: Faker::Quote.most_interesting_man_in_the_world,
+    content: Faker::Movie.quote
+  )
+  article.save
+  puts "Article with id #{article.id} was created"
+end
